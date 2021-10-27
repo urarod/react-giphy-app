0924b7ca66f28aa398f87bb682b280d60955d748
urarod <fcuentasmimportantes@gmail.com>
2021-10-27 16:33:08 -0500

    fixed imports and exports

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import { useState, useEffect } from "react";
 import getGifs from "../helpers/getGifs";
 
 const useFetchGifs = (category) => {
